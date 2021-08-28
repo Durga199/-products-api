f81c2a11cd63c459721f4991fbbc8e8cc018d459
Durga prasad <kardurgaprasad1997@gmail.com>
2021-08-28 11:26:30 +0530

    Project Done

--- products/views.py
+++ products/views.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 from django.http import HttpResponse,JsonResponse
 from .models import *
 from .serializer import *
-import pprint
 from django.core import serializers
 import json
 
